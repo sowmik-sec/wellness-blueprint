30c8fd1c0e23d36b1a25958cd8a2ba29b7a98ee4
sowmik-sec <sowmik.sec@gmail.com>
2024-07-11 21:34:40 +0600

    create navbar

---
+++ src/shared/Navbar.jsx
@@ -0,0 +1,75 @@
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+
+function Navbar() {
+  const { user } = useContext(AuthContext);
+  const links = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/events"}>Events</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/about"}>About Us</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <Link to={"/"} className="btn btn-ghost text-xl">
+          Wellness Blueprint
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        {user ? (
+          <>
+            <div>
+              <img
+                className="rounded-full w-10 h-10 mr-2"
+                title={user.displayName}
+                src={user.photoURL}
+                alt=""
+              />
+            </div>
+            <button>Sign Out</button>
+          </>
+        ) : (
+          <Link to={"/login"}>Login</Link>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
