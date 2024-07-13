9f396e530a4afc1f0c14e28065d4d2746185a0f4
sowmik-sec <sowmik.sec@gmail.com>
2024-07-13 12:04:58 +0600

    create event card

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,7 +1,27 @@
+import { useLoaderData } from "react-router-dom";
+import Event from "./Event";
+import { useState } from "react";
+
 function Events() {
+  const events = useLoaderData();
+  const [show, setShow] = useState(4);
+
+  console.log(events);
   return (
     <div>
-      <h2 className="text-3xl">Event page</h2>
+      <h2 className="text-3xl text-center">These are our Events</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-14">
+        {events.slice(0, show).map((event) => (
+          <Event key={event.id} event={event} />
+        ))}
+      </div>
+      {show === 4 && (
+        <div className="flex justify-center mb-3">
+          <button onClick={() => setShow(6)} className="btn btn-primary">
+            See All Events
+          </button>
+        </div>
+      )}
     </div>
   );
 }
